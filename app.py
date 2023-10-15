c60078dea25f8757bceaa28daf2968e60ac11f0e
Olivier Youmbi <110872420+MES-DASH-B@users.noreply.github.com>
2023-10-15 15:03:31 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
+
+
+
+
+
+
 PAGE_TITLE = "Digital CV  YOUMBI OLIVIER"
 PAGE_ICON = ":wave"
 NAME = "OLIVIER YOUMBI"
@@ -43,13 +49,13 @@ with col2:
         mime="application/octet-stream",
     )
     st.write("📧", EMAIL) 
-    cols = st.columns(len(PROJETS))
-        for index, (platform, link) in enumerate(PROJETS.items()):
-        cols  [index].write(f"[{platform}]({link})")
+    st.write("🔗 LinkedIn", LinkedIn ) 
 
 st.write("#")
+cols = st.columns(len(PROJETS))
+for index, (platform, link) in enumerate(PROJETS.items()):
+    cols  [index].write(f"[{platform}]({link})")
 
-st.write("🔗 LinkedIn", LinkedIn ) 
 st.write("#")
 st.subheader("Experience & Qualifications")
 st.write (
