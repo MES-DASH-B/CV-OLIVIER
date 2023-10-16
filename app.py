db82e82d0d0e4ed757e11f2be727f829a79fb395
Olivier Youmbi <110872420+MES-DASH-B@users.noreply.github.com>
2023-10-16 09:16:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ ASPIRANT MANAGER DE PROJET D'INNOVATION, INDUSTRIE 4.0/RECHERCHE D'ALTERNANCE (C
 EMAIL = "youmbio@yahoo.com"
 LinkedIn = "https://www.linkedin.com/in/ornello-olivier-youmbi-1a66b1184/"
 Projet_1 = "https://abc-cocacola-lytics-03.streamlit.app/"
-Projet_2 = "https://maint-cocacolaxyz.streamlit.app/"
+Projet_2 = "https://maint-cocacola.streamlit.app/"
 
 #PROJETS = {
     #"Projet 1 - description du projet": "https://ibs-cocacola-lytics-03.streamlit.app/",
